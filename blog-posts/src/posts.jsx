e1e253f3650d9e01d315a5ee5a12e4bcbc2c717c
Tshireletso <tmogwere09@gmail.com>
2024-06-11 10:22:33 +0200

    conditional statements to check if respnse status is okay else throws error

--- blog-posts/src/posts.jsx
+++ blog-posts/src/posts.jsx
@@ -10,6 +10,9 @@ function Posts() {
         const fetchPosts = async () => { //handles data fetch
             try { //statrs try block
                 const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+                if (!response.ok) { 
+                    throw new Error('Failed to fetch posts');
+                }
             }
         }
     }
