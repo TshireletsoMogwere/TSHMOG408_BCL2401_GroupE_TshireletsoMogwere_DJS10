058a8a00b9eacd0422dd57d7ad37cde60c29362a
Tshireletso <tmogwere09@gmail.com>
2024-06-11 10:01:59 +0200

    stores error messages if data fetching fails

--- blog-posts/src/posts.jsx
+++ blog-posts/src/posts.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 function Posts() {
     // Initialize state to empty array for independency
     const [posts, setPosts] = useState([]);
+    // Initializes storing of error messages when data fetching fails
+    const [error, setError] = useState(null)
     return (
         <>
         </>
